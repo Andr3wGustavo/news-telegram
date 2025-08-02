b83ea593b10744831bc98b323f1c6d43d29846f2
ettios237 <andreadgustavo@gmail.com>
2025-08-02 00:52:20 -0300

    add config file

---
+++ config.py
@@ -0,0 +1,37 @@
+TELEGRAM_TOKEN = "8425822040:AAEfkvFb2tME8VsrjcimrHKlS31OlZExbEM"
+TELEGRAM_CHAT_ID = "7824852023" 
+GEMINI_API_KEY = "" 
+
+
+
+# Lista de feeds RSS para monitorar
+# Adicione ou remova fontes conforme seu desejo
+RSS_FEEDS = {
+    #"TechCrunch": "http://feeds.feedburner.com/TechCrunch/",
+    #"CoinDesk": "https://www.coindesk.com/arc/outboundfeeds/rss/",
+    #"Reuters Biz": "https://www.reuters.com/pf/api/v2/content/common/feeds/rss/business",
+    #"Ars Technica": "http://feeds.arstechnica.com/arstechnica/index",
+    #"Verge": "https://www.theverge.com/rss/index.xml",
+    #"globo": "https://g1.globo.com/rss/g1/tecnologia/"
+
+    #cripto RSS
+    "CoinDesk": "https://www.coindesk.com/arc/outboundfeeds/rss/",
+    "Cointelegraph": "https://cointelegraph.com/rss",
+    "The Block": "https://www.theblock.co/rss.xml",
+    "Decrypt": "https://decrypt.co/feed",
+    "Bitcoin Magazine": "https://bitcoinmagazine.com/feed",
+    "U.Today": "https://u.today/rss",
+    "CryptoSlate": "https://cryptoslate.com/feed/",
+    "BeInCrypto": "https://beincrypto.com/feed/",
+
+    #economia RSS
+    "Reuters Business": "https://www.reuters.com/pf/api/v2/content/common/feeds/rss/business",
+    "Reuters World": "https://www.reuters.com/pf/api/v2/content/common/feeds/rss/world",
+    "Wall Street Journal World": "https://feeds.a.dj.com/rss/RSSWorldNews.xml",
+    "Wall Street Journal Markets": "https://feeds.a.dj.com/rss/RSSMarketsMain.xml",
+    "Financial Times World": "https://www.ft.com/world?format=rss",
+    #"Bloomberg Markets": "https://feeds.bloomberg.com/markets/news.rss",
+    "The Economist": "https://www.economist.com/finance-and-economics/rss.xml",
+    #"Forbes": "https://www.forbes.com/business/feed/",
+    "Harvard Business Review": "https://hbr.org/rss/regular",
+}
